33664c0051a28ecd0124b6d5e8b0dae7376dec4a
Welinton Suarez <wesgdeveloper@gmail.com>
2024-04-23 14:32:34 -0500

    Add image hover effect to ProductGridItem component

--- src/components/products/products-grids/ProductGridItem.tsx
+++ src/components/products/products-grids/ProductGridItem.tsx
@@ -1,21 +1,27 @@
+"use client";
+
 import { Product } from "@/interfaces";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 interface Props {
   product: Product;
 }
 
 export const ProductGridItem = ({ product }: Props) => {
+  const [displayImage, setDisplayImage] = useState(product.images[0]);
   return (
     <div className="rounded-md overflow-hidden fade-in">
       <Link href={`/product/${product.slug}`}>
         <Image
-          src={`/products/${product.images[0]}`}
+          src={`/products/${displayImage}`}
           alt={product.title}
           width={500}
           height={500}
-          className="object-cover w-full"
+          className="object-cover w-full rounded"
+          onMouseEnter={() => setDisplayImage(product.images[1])}
+          onMouseLeave={() => setDisplayImage(product.images[0])}
         />
       </Link>
 
